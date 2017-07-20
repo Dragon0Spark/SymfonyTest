863e2836f466ea65bdfa7545dc728bde7f4ab2ad
Dragon0Spark <thelastblade@live.fr>
2017-07-20 12:38:04 +0200

    V1.0 released

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
-Symfony
-=======
+# BonoboProject
 
-A Symfony project created on July 9, 2017, 2:04 pm.
+Gestion d'une liste d'amis pour bonobos (singes intelligents) en Symfony 3
+
+## Pour commencer
+
+Avoir installer Composer sur sa machine
+
+
+### Pour démarrer
+
+
+Configurer votre base de données à l'aide du fichier [parameters.yml](https://github.com/Brayan233/BonoboProject/blob/master/app/config/parameters.yml)
+
+```
+# EXEMPLE
+parameters:
+    database_host: 127.0.0.1
+    database_port: null
+    database_name: BonoboProject
+    database_user: root
+    database_password: root
+    mailer_transport: smtp
+    mailer_host: 127.0.0.1
+    mailer_user: null
+    mailer_password: null
+secret: bf4be6ffc923ed9031f725ce43af2170b6d1467f
+
+```
+
+Créer la BDD que vous avez configuré précédement
+
+
+Installer/Mettre à jour toutes les dépendances (dont FOSUSERBUNDLE),à l'aide de Composer à la racine du projet
+
+```
+composer update
+```
+
+Générer les tables pour la BDD
+
+```
+php bin/console doctrine:schema:update --force
+```
+
+Remplir la BDD avec une liste de Bonobos pré-enregistré
+
+```
+php bin/console doctrine:fixtures:load
+```
+
+Démarrer le serveur
+
+```
+php bin/console server:start
+```
+
+ENJOY vous pouvez [démarrer](http://localhost:8000)
+
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,8 +1,11 @@
 <?php
 // src/AppBundle/Entity/User.php
+
 namespace AppBundle\Entity;
+
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
@@ -15,47 +18,56 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+
+
+    /**
+     *
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Friend", cascade={"persist"})
+     *   
+     */
+
+    private $friends;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
     }
-	
-	/**
-    * @ORM\OneToMany(targetEntity="...\Entity\Friend", inversedBy="user")
-    */
-	protected $friends;
-     
+
+
+
     /**
-     * Add friends
+     * Add friend
      *
-     * @param ...\Friend $friends
-     * @return user
+     * @param \AppBundle\Entity\Friend $friend
+     *
+     * @return User
      */
-    public function addFriend(\...\Entity\Friend $friends)
+    public function addFriend(\AppBundle\Entity\Friend $friend)
     {
-        $this->friends[] = $friends;
-     
+        $this->friends[] = $friend;
+
         return $this;
     }
-     
+
     /**
-     * Remove friends
+     * Remove friend
      *
-     * @param ...\Entity\Friend $friends
+     * @param \AppBundle\Entity\Friend $friend
      */
-    public function removeFriend(\...Entity\Friend $friends)
+    public function removeFriend(\AppBundle\Entity\Friend $friend)
     {
-        $this->friends->removeElement($friends);
+        $this->friends->removeElement($friend);
     }
-     
+
     /**
      * Get friends
      *
-     * @return Doctrine\Common\Collections\Collection
+     * @return \Doctrine\Common\Collections\Collection
      */
     public function getFriends()
     {
         return $this->friends;
-    } 
+    }
 }
